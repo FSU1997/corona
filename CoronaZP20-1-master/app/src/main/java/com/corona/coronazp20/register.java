1224a925b8f52d46f0f9df940c3b7e9b896ebf04
FSU1997 <70893880+FSU1997@users.noreply.github.com>
2020-10-05 15:16:32 +0300

    Add files via upload

---
+++ CoronaZP20-1-master/app/src/main/java/com/corona/coronazp20/register.java
@@ -0,0 +1,17 @@
+package com.corona.coronazp20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+
+public class register extends AppCompatActivity {
+    private EditText textInput
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
